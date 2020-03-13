5d2700734acf46f9c9937de10a10082ebea98d2b
João Medeiros <33334915+Jaum97@users.noreply.github.com>
2020-03-12 23:34:00 -0300

    Update operators.ts

--- operators.ts
+++ operators.ts
@@ -6,3 +6,15 @@ export const pipe: F.Pipe = function(...fns) {
 		return fns.reduce((currentVal, currentFn) => currentFn(currentVal) , value)
 	}
 }
+
+export function curry(func) {
+  return function curried(...args) {  
+    if (args.length >= func.length) {    
+      return func.apply(this, args);   
+    } else {   
+      return function(...args2) {
+        return curried.apply(this, args.concat(args2));
+      }   
+    }  
+  };
+}
